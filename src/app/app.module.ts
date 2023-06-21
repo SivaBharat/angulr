51372acbbb3f992c11986f41633afcfcaefd6e93
SivaBharat <sivabharthece@gmail.com>
2023-06-21 11:15:03 +0530

    formsCommit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from "@angular/platform-browser";
 import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
-  
+import { ReactiveFormsModule } from '@angular/forms';  
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -43,7 +43,8 @@ import { FormsModule } from '@angular/forms';
     ImageModule,
     InputTextModule,
     ButtonModule,
-    FormsModule
+    FormsModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/reactive/reactive.component.ts
+++ src/app/reactive/reactive.component.ts
@@ -1,10 +1,36 @@
-import { Component } from '@angular/core';
+import { Component ,OnInit} from '@angular/core';
+import { FormGroup,FormControl, Form } from '@angular/forms';
+import { Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-reactive',
   templateUrl: './reactive.component.html',
   styleUrls: ['./reactive.component.css']
 })
-export class ReactiveComponent {
+export class ReactiveComponent implements OnInit{
+react_form!:FormGroup;
+username:FormControl|any;
+password:FormControl|any
 
+  ngOnInit(): any {
+    this.username = new FormControl('', [
+      Validators.required,
+      Validators.minLength(10),
+      Validators.maxLength(16)
+    ]);
+    this.password = new FormControl('', [
+      Validators.required,
+      Validators.minLength(8),
+      Validators.maxLength(16)
+    
+  ])
+    this.react_form = new FormGroup({
+      username: this.username,
+      password:this.password,
+      
+    });
+  }
+  onSubmit(form:any){
+    console.log(form.value)
+  }
 }
